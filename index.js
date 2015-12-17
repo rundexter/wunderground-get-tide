c3624141abee6dacc15cadc81bc84fe579073eb0
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-17 15:30:25 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,103 @@
+var _ = require('lodash'),
+    request = require('request'),
+    util = require('./util.js');
+
+var apiURL = 'http://api.wunderground.com/api/';
+
+var pickInputs = {
+        'country': 'country',
+        'city': 'city',
+        'state': 'state'
+    },
+    pickOutputs = {
+        'tideInfo_tideSite': {
+            keyName: 'tide.tideInfo',
+            fields: ['tideSite']
+        },
+        'tideInfo_units': {
+            keyName: 'tide.tideInfo',
+            fields: ['units']
+        },
+        'tideInfo_type': {
+            keyName: 'tide.tideInfo',
+            fields: ['type']
+        },
+        'tideInfo_tzname': {
+            keyName: 'tide.tideInfo',
+            fields: ['tzname']
+        },
+
+        'tideSummary_date_pretty': {
+            keyName: 'tide.tideSummary',
+            fields: ['date.pretty']
+        },
+
+        'tideSummaryStats_maxheight': {
+            keyName: 'tide.tideSummaryStats',
+            fields: ['maxheight']
+        },
+        'tideSummaryStats_minheight': {
+            keyName: 'tide.tideSummaryStats',
+            fields: ['minheight']
+        }
+    };
+
 module.exports = {
+
+    /**
+     * Get auth data.
+     *
+     * @param dexter
+     * @returns {*}
+     */
+    authorizeRequest: function (dexter) {
+
+        if(!dexter.environment('wunderground_api_key')) {
+
+            this.fail('A [wunderground_api_key] environment variable is required for this module');
+
+            return false;
+        } else {
+
+            request = request.defaults({
+                baseUrl: apiURL.concat(_(dexter.environment('wunderground_api_key')).toString().trim())
+            });
+
+            return true;
+        }
+    },
+
+    /**
+     * Check correct inputs data.
+     *
+     * @param step
+     * @param pickInputs
+     * @returns {*}
+     */
+    checkInputStruct: function (step, pickInputs) {
+        var requestData = util.pickStringInputs(step, pickInputs),
+            uriData = [];
+
+        if (requestData.country)
+            uriData.push(requestData.country);
+
+        if (requestData.city)
+            uriData.push(requestData.city);
+
+        if (requestData.state)
+            uriData.push(requestData.state);
+
+
+        if (uriData.length < 2) {
+
+            this.fail('A [country,city or state] inputs need for this module.');
+
+            return false;
+        }
+
+        return uriData;
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +105,27 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+
+        var uriData = this.checkInputStruct(step, pickInputs),
+            uri = 'tide/q/' + uriData.join('/') + '.json';
+
+        if (!this.authorizeRequest(dexter) || !uriData)
+            return;
+
+
+        request.get({uri: uri, json: true}, function (error, response, data) {
+            console.log(data.tide.tideInfo[0].tideSite);
+            if (error)
+                this.fail(error);
+
+            else if (data.error)
+                this.fail(data.error);
+
+            else if (response.statusCode !== 200)
+                this.fail(response.statusCode + ': Something is happened');
+
+            else
+                this.complete(util.pickResult(data, pickOutputs));
+        }.bind(this));
     }
 };
